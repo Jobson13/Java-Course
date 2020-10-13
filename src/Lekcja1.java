411c2bc0edd5e097dd9198cad2ecaae60d080b8a
Jobson13 <50521404+Jobson13@users.noreply.github.com>
2020-10-13 19:55:48 +0200

    Lesson 1

--- src/Lekcja1.java
+++ src/Lekcja1.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Lekcja1 {
-    public static void main
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.print("Podaj swoje imię: ");
+        name = scanner.next();
+        System.out.print("Hello " + name);
+    }
 }
